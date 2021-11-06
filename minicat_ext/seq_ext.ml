378e73587c6c26a9ab89d6d39da91595e29b740f
Nathan Graule <solarliner@gmail.com>
2021-11-06 18:07:56 +0100

    fix: use `Seq.append` to remove dependency on OCaml 4.13

--- minicat_ext/seq_ext.ml
+++ minicat_ext/seq_ext.ml
@@ -8,7 +8,7 @@ let app f xs = Seq.flat_map (fun f -> Seq.flat_map (fun x -> pure (f x)) xs) f
 
 let bind m f = Seq.flat_map f m
 
-let alt a b = Seq.concat (Seq.cons a (Seq.cons b empty))
+let alt = Seq.append
 
 let rec fold_right f l i =
   match l () with Seq.Nil -> i | Seq.Cons (x, xs) -> f x (fold_right f xs i)
